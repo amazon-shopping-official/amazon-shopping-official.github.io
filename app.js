018469a2af58af24a5f09f1b0ac088fc5c6d83f6
DANIELRAAJ <raajdaniel14@gmail.com>
2026-09-15 12:42:23 +0530

    Configure for amazon-shopping-official.github.io

--- app.js
+++ app.js
@@ -59,11 +59,11 @@ function getDynamicOrderDate(date = new Date()) {
 // ==========================================================================
 //  GITHUB REPO ORDERS PERSISTENCE
 //  Commits and reads order details directly to 'orders.json' in GitHub:
-//  https://github.com/amazon-shopping-cloud/amazon-shopping-cloud.github.io/blob/main/orders.json
+//  https://github.com/amazon-shopping-official/amazon-shopping-official.github.io/blob/main/orders.json
 // ==========================================================================
 const GH_CONFIG = {
-  owner: (window.location.hostname && window.location.hostname.includes("amazon-shopping-cloud")) ? "amazon-shopping-cloud" : "amazonshoppingintl-cloud",
-  repo: "amazon-shopping-cloud.github.io",
+  owner: (window.location.hostname && window.location.hostname.includes("amazon-shopping-official")) ? "amazon-shopping-official" : "amazonshoppingintl-cloud",
+  repo: "amazon-shopping-official.github.io",
   filePath: "orders.json",
   getAuth: function() {
     // Obfuscated string chunks to prevent automated regex scanner false-positive revocation
